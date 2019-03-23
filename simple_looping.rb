7bee7c78a70f1ed592cb9eb3663f0eaf020a5267
_ _ <mkiyani007@gmail.com>
2019-03-23 01:47:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,8 @@ def loop_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
    loop do
+     counter =+ 1 
+     
 end
 
 def times_iterator(number_of_times)
